901dbc74d1e2d7b98bd28a52d63c8a6527759125
bharatgupta77 <guptabharat297@gmail.com>
2023-01-06 17:27:45 +0530

    refactored otp_generator code

--- otp_generator.py
+++ otp_generator.py
@@ -4,16 +4,18 @@ import random as r
 randint() is an inbuilt function of the random module in Python3. The random module gives access to various useful functions and one of them being able to generate random numbers, which is randint().
 """
 
-# function for otp generation
+# function for otp generation. OTP of length n will be generated.
 
 
-def otp_generation():
+def otp_generation(n):
     otp_l = ""
-    # length of otp is 6
-    for i in range(6):
+    for i in range(n):
         otp_l += str(r.randint(0, 9))
     return otp_l
 
 
-otp = otp_generation()
+print("Enter the length of OTP you want :")
+n = int(input())
+
+otp = otp_generation(n)
 print("Your one time password is : "+otp)

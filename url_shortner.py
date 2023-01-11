66f89d7b0f0ad983bfa8411014d6c8378c9c2a14
bharatgupta77 <guptabharat297@gmai.com>
2023-01-11 21:33:19 +0530

    added url shortner code

--- url_shortner.py
+++ url_shortner.py
@@ -1,6 +1,6 @@
 import bitly_api
 
-# bitly access token - please add your own api access token below
+# bitly access token - please add your own api access token below ""
 access_Token = ""
 
 # establish a connection with bitly_api
@@ -9,7 +9,7 @@ connection = bitly_api.Connection(access_token=access_Token)
 # take the url from user
 url = input("Enter the url you wish to shorten: ").strip()
 
-#to shorten the url, using shorten method
+#to shorten the url, using shorten method provided by bitly
 shorten_url = connection.shorten(url)
 
 # print shorten url

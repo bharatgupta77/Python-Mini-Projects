18d268b9cf7ed98d7c08f04cfcbfcaabf7b47da6
bharatgupta77 <guptabharat297@gmail.com>
2023-01-21 01:03:46 +0530

    added new code

--- currency_converter.py
+++ currency_converter.py
@@ -1,7 +1,23 @@
 import currency_converter
+import tkinter as t
 
-current_currency = input("Enter the current currency: ")
-new_currency = input("Enter the currency you want to convert into : ")
+# create the main window
+window = t.Tk(screenName=None, baseName=None, className='window', useTk=1)
+'''
+widgets are added here
+'''
+title = t.Label(text="Welcome to Currency Converter tool ")
+title.pack()
+
+window.mainloop()
+
+
+
+
+
+
+#current_currency = input("Enter the current currency: ")
+#new_currency = input("Enter the currency you want to convert into : ")
 
 
 
